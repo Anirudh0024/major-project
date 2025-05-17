495a8ecf4697117993790a08762936c223a41ece
Anirudh <anirudhapratapsingh@Mac.bbrouter>
2025-05-18 00:43:31 +0530

    changed

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 
 console.log("Server starting lalala"); 
 
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose")
-const MONGO_URL = "mongodb://127.0.0.1:27017/wandurlust"
-const Listing = require("./models/listing.js")
-const path = require("path");
-const methodOverride = require("method-override")
-const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js")
-const ExpressError = require("./utils/ExpressError.js")
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
