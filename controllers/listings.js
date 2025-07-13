97c43ae5d9f74d7e3d2d565657d4a02c82d4bbcb
Anirudh <anirudhapratapsingh@Anirudhas-MacBook-Air.local>
2025-07-13 08:26:56 +0530

    changes made

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,63 +1,66 @@
-const Listing=require("../models/listing")
+const Listing = require("../models/listing");
 
-module.exports.index=async (req, res) => {
-    const alllistings = await Listing.find({});
-    res.render("listings/index.ejs", { alllistings });
-  }
+module.exports.index = async (req, res) => {
+  const alllistings = await Listing.find({});
+  res.render("listings/index.ejs", { alllistings });
+};
 
-  module.exports.renderNewForm=(req, res) => {
-    console.log(req.user);
-    res.render("listings/new.ejs");
-  }
+module.exports.renderNewForm = (req, res) => {
+  console.log(req.user);
+  res.render("listings/new.ejs");
+};
 
-  module.exports.showListing=async (req, res) => {
-    let { id } = req.params;
-    try {
-      const listing = await Listing.findById(id)
-        .populate({path:"reviews",populate :{
-          path:"author",
-        }})
-        .populate("owner");
-      if (!listing) {
-        req.flash("error", "Listing You requested for does not exist!");
-        res.redirect("/listings");
-      }
-      console.log(listing);
-      res.render("listings/show.ejs", { listing });
-    } catch (err) {
-      res.render("404.ejs");
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  try {
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("owner");
+    if (!listing) {
+      req.flash("error", "Listing You requested for does not exist!");
+      res.redirect("/listings");
     }
+    console.log(listing);
+    res.render("listings/show.ejs", { listing });
+  } catch (err) {
+    res.render("404.ejs");
   }
+};
 
-  module.exports.createListing=async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    console.log(req.user)
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "New Listing Created");
-    res.redirect("/listings");
-  }
-  module.exports.renderEditForm=async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  }
+module.exports.createListing = async (req, res, next) => {
+  const newListing = new Listing(req.body.listing);
+  console.log(req.user);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New Listing Created");
+  res.redirect("/listings");
+};
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+};
 
-  module.exports.updateListing=async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("success", "Listing Updated!");
-    res.redirect(`/listings/${id}`);
-  }
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", "Listing Updated!");
+  res.redirect(`/listings/${id}`);
+};
 
-  module.exports.deleteListing=async (req, res) => {
-    console.log(
-      `DELETE /listings/:id - req.path: ${req.path}, req.params:`,
-      req.params
-    );
-    let { id } = req.params;
-    let deletedLsting = await Listing.findByIdAndDelete(id);
-    console.log(deletedLsting);
-    req.flash("success", "Listing Deleted!");
-    res.redirect("/listings");
-  }
+module.exports.deleteListing = async (req, res) => {
+  console.log(
+    `DELETE /listings/:id - req.path: ${req.path}, req.params:`,
+    req.params
+  );
+  let { id } = req.params;
+  let deletedLsting = await Listing.findByIdAndDelete(id);
+  console.log(deletedLsting);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
--- routes/review.js
+++ routes/review.js
@@ -4,7 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const {validateReview, isLoggedIn, isreviewAuthor}=require("../middleware.js")
-
+const reviewController=require("../controllers/reviews.js")
 
 
   
@@ -15,30 +15,13 @@ router.post(
     "/",
     isLoggedIn,
     validateReview,
-    wrapAsync(async (req, res) => {
-        console.log(req.params.id)
-      let listing = await Listing.findById(req.params.id);
-      let newReview = new Review(req.body.review);
-      newReview.author=req.user._id;
-      console.log(newReview)
-      listing.reviews.push(newReview);
-      await newReview.save();
-      await listing.save();
-      req.flash("success","Review Created!")
-      res.redirect("/listings/");
-    })
+    wrapAsync(reviewController.createReview)
   );
   
   // delete
   router.delete(
     "/:reviewId",isLoggedIn,isreviewAuthor,
-    wrapAsync(async (req, res) => {
-      let { id, reviewId } = req.params;
-      await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-      await Review.findByIdAndDelete(reviewId);
-      req.flash("success","Review Deleted!")
-      res.redirect(`/listings/${id}`);
-    })
+    wrapAsync(reviewController.deleteReview)
   );
 
   module.exports=router;
